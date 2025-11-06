9c1bfaccd3663af096193c26bc4a76c4322db0c7
romuloloesch <romuloloesch83@gmail.com>
2025-11-06 12:46:41 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
-/** @type {import('next').NextConfig} */
+//** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true }
+  experimental: {
+    appDir: true,
+  },
+
+  async redirects() {
+    return [
+      // 🔁 Redireciona todo o tráfego do domínio Vercel para o domínio principal
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'loesch-oraculo-site.vercel.app' }],
+        destination: 'https://loescheoraculo.com/:path*',
+        permanent: true, // 308 - redirect permanente
+      },
+    ];
+  },
 };
+
 export default nextConfig;
